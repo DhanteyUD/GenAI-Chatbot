3755fa84b5a98908732ba5cfedb13de2e361ac36
Clinton Otse <otseclinton60@gmail.com>
2025-03-30 20:02:37 +0100

    Chatbot

---
+++ chatbot.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# Upload PDF files
+st.header("UD Chatbot")
+
+with st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader("Upload a PDF file and start asking questions", type="pdf")
+
+# Extract text
+if file is not None:
+    pdf_Reader = PdfReader(file)
+    text = ""
+    for page in pdf_Reader.pages:
+        text += page.extract_text()
+        # st.write(text)
+
+# Breaking text into chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n"],
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text);
+    # st.write(chunks)
